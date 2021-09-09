5d40b87eaf5877f1f6255cdd68bcf87bfff9749e
PaulMed <paulmedeiros92@gmail.com>
2021-09-08 19:29:25 -0700

    Set up firebase

--- src/listeners/ready-listener.js
+++ src/listeners/ready-listener.js
@@ -1,4 +1,17 @@
 const { Listener } = require('discord-akairo');
+const { initializeApp } = require("firebase/app");
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBO6KxdWW1iDA3J3iB3B8ztwmggzk7sBbM",
+  authDomain: "animebot-64e6a.firebaseapp.com",
+  projectId: "animebot-64e6a",
+  storageBucket: "animebot-64e6a.appspot.com",
+  messagingSenderId: "1092288496849",
+  appId: "1:1092288496849:web:d22818bc1e67ac96c571f0",
+  measurementId: "G-M6YBNFZ75C"
+};
 
 class ReadyListener extends Listener {
     constructor() {
@@ -9,7 +22,14 @@ class ReadyListener extends Listener {
     }
 
     exec() {
-        console.log("I'm ready!");
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
+
+        const serversCol = collection(db, 'shows');
+        getDocs(serversCol).then( servers => {
+            servers.docs.forEach(doc => console.log(doc.data()));
+            console.log("I'm ready!");
+        });
     }
 }
 
